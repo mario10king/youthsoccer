ebd6a9225a6ef713778e87961afad4b858b666d4
Mario Marroqin <mmarroking@gmail.com>
2015-01-16 12:22:59 -0800

    Seeded database with coast teams

--- app/controllers/ranking_controller.rb
+++ app/controllers/ranking_controller.rb
@@ -1,6 +1,3 @@
-require 'nokogiri'
-require 'open-uri'
-
 class RankingController < ApplicationController
   def teams
   end
@@ -11,74 +8,19 @@ class RankingController < ApplicationController
     elsif params[:league] == "scsdl"
       scsdl
     else
-    both
+      both
     end
     render "ranking/coast"
     
     
   end
   def coast
-    squad = []
-    
-    age = @age
-    gender = {boys: "SB", girls: "SG"} 
-    bracket = {pr: "0P", g: "1G", gn: "1N", gs: "1S", ses: "S1", sen: "S2", sew: "S3", see: "S4", 
-             sa: "SA", sc: "SC", sd: "SD", se: "SE", sn: "SN", ss: "SS", sw: "SW",
-             za: "ZA", zb: "ZB", zc: "ZC", zd: "ZD", ze: "ZE", zg: "ZG", zk: "ZK", zn: "ZN",
-             zo: "ZO", zs: "ZS", zw: "ZW", zz: "ZZ"}
-    bracket.each do |key, val|
-    if age.to_i >= 10
-      page = Nokogiri::HTML(open("http://www.coastsoccer.com/2014/#{gender[:boys]}#{age}#{val}.htm"))
-    else
-      page = Nokogiri::HTML(open("http://www.coastsoccer.com/2014/#{gender[:boys]}#{"0"+age}#{val}.htm"))
-    end
-    teams = page.css("table.StandTable td.StandTeam")
-    list = []
-    teams.each do |team|
-      list << team.text.gsub(/\302\240/,"").strip
-    end
-    stats = page.css("table.StandTable td.StandCell")
-    i = 0
-    x = 0
-    games = 0
-    gp = []
-    while i < list.length * 6
-      games  += stats[i].text.gsub(/\302\240/,"").strip.to_i
-      x+=1
-      if x % 3 == 0
-         
-        ppg = (stats[i+1].text.gsub(/\302\240/,"").strip.to_i / games.to_f).round(2)
-        if val == "1G" || val == "1N" || val == "1S" 
-          gp << ppg - 1
-        elsif val == "S1" || val == "S2" || val == "S3" || val == "S4"  
-          gp << ppg - 2
-        elsif val[0] == "S"  
-          gp << ppg - 3
-        elsif val[0] == "Z" 
-          gp << ppg - 4
-        else 
-          gp << ppg
-        end
-        i += 4 
-        games = 0
-      else
-        i+=1
-      end
-    end
-    arr = list.zip(gp)
+  
+    @squad = Team.where(age: @age).order(ppg: :desc)
 
-    arr.each do |team|
-      squad << team
-    end
-    end
-      squad.each do |team|
-    if team[1].nan?
-    squad.delete(team)
-    end 
-  end
-    @squad = squad.sort {|a,b| b[1] <=> a[1]}
    # render 'ranking/coast'
   end
+
   def scsdl
     require 'json'
 
@@ -1335,17 +1277,17 @@ case @age.to_i
   when 11
   b_11.each do |team|
  av = team["tgnm"].split(" ")
-  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - av[4].to_i + 1]
+  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - av[4].to_i]
 end
   when 12
   b_12.each do |team|
  av = team["tgnm"].split(" ")
-  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - (av[4].to_i) + 1]
+  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - (av[4].to_i)]
 end
   when 13
   b_13.each do |team|
  av = team["tgnm"].split(" ")
-  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - (av[4].to_i) + 1]
+  arr << [team["tmnm"],team["AVG_PTS_PER_GM"].to_f - (av[4].to_i)]
 end
   when 14
   b_14.each do |team|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,63 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Club.create([{name: "FCLA", location: "westside"},{name: "LAUFA" ,location: "hollywood"},{name: "real so cal" ,location: "valley"},{name: "strikers" ,location: "irvine"},{name: "surf" ,location: "san diego"},{name: "valley united",location: "valley"}])
+require 'nokogiri'
+require 'open-uri'
 League.create([{name: "SCSDL"},{name: "Presidio"},{name: "Coast"}])
-20.times do
-  club = rand(5) + 1
-  age =  8 + rand(10)
-  Team.create([{name: "#{Club.find(club).name} #{age}", league_id: rand(3), club_id: club, age: age , coach: "Juan", rank: rand(100)}])
-end
-100.times do
-  Player.create([{team_id: rand(20), name: "pablo", birthday: "#{rand(13)}/#{rand(28)}/#{rand(100)}" }])
+a = 11
+while a <= 17 /
+  squad = []
+  age = a
+  gender = {boys: "SB", girls: "SG"} 
+  bracket = {pr: "0P", g: "1G", gn: "1N", gs: "1S", ses: "S1", sen: "S2", sew: "S3", see: "S4", 
+  sa: "SA", sc: "SC", sd: "SD", se: "SE", sn: "SN", ss: "SS", sw: "SW",
+  za: "ZA", zb: "ZB", zc: "ZC", zd: "ZD", ze: "ZE", zg: "ZG", zk: "ZK", zn: "ZN",
+  zo: "ZO", zs: "ZS", zw: "ZW", zz: "ZZ"}
+  bracket.each do |key, val|
+    page = Nokogiri::HTML(open("http://www.coastsoccer.com/2014/#{gender[:boys]}#{age}#{val}.htm"))
+    teams = page.css("table.StandTable td.StandTeam")
+    list = []
+    teams.each do |team|
+      list << team.text.gsub(/\302\240/,"").strip
+    end
+    stats = page.css("table.StandTable td.StandCell")
+    i = 0
+    x = 0
+    games = 0
+    gp = []
+    while i < list.length * 6
+      games  += stats[i].text.gsub(/\302\240/,"").strip.to_i
+      x+=1
+      if x % 3 == 0
+        ppg = (stats[i+1].text.gsub(/\302\240/,"").strip.to_i / games.to_f).round(2)
+        if val == "1G" || val == "1N" || val == "1S" 
+          gp << ppg - 1
+        elsif val == "S1" || val == "S2" || val == "S3" || val == "S4"  
+          gp << ppg - 2
+        elsif val[0] == "S"  
+          gp << ppg - 3
+        elsif val[0] == "Z" 
+          gp << ppg - 4
+        else 
+          gp << ppg
+        end
+        i += 4 
+        games = 0
+      else
+        i+=1
+      end
+    end
+  arr = list.zip(gp)
+  arr.each do |team|
+    squad << team
+  end
+  end
+  squad.each do |team|
+    if team[1].nan?
+      squad.delete(team)
+    else
+      Team.create([{name: team[0], league_id: 3, age: age , ppg: team[1] }])
+    end 
+  end
+  a += 1
 end
